481b707ced37b040c80b95bda0d9ff37188b0e92
donghyun <godh1234@naver.com>
2020-04-29 16:18:03 +0900

    메인함수 Version 변경

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import time
 import sys
 import main as craps
 
-build_version = '4.2.0'
+build_version = '4.2.1'
 current_time = time.asctime(time.localtime(time.time()))
 print("\n\nBuild Version:", build_version,
       "\nTime:", str(current_time), "\n\n")
